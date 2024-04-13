edbb4a361cb002a1871d28ad39d3c220d5bfad50
JiauZhang <1743960454@qq.com>
2024-04-13 13:49:20 +0800

    update

--- setup.py
+++ setup.py
@@ -118,30 +118,6 @@ class CMakeBuild(build_ext):
         )
 
 setup(
-    name = 'datamana',
-    packages = find_packages(exclude=['examples']),
-    version = '0.0.2',
-    license='MIT',
-    description = 'Dataset Manager',
-    author = 'JiauZhang',
-    author_email = 'jiauzhang@163.com',
-    url = 'https://github.com/JiauZhang/datamana',
-    long_description=open("README.md", "r", encoding="utf-8").read(),
-    long_description_content_type = 'text/markdown',
     ext_modules=[CMakeExtension("datamana.sem")],
     cmdclass={"build_ext": CMakeBuild},
-    keywords = [
-        'Deep Learning',
-        'Dataset Manager',
-        'Artificial Intelligence',
-    ],
-    install_requires=[
-        'numpy',
-    ],
-    classifiers=[
-        'Intended Audience :: Developers',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.8',
-    ],
 )
