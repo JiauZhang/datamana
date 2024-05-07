02ed67032db26d2f3110b45e7b1ef68fb02bf116
JiauZhang <1743960454@qq.com>
2024-05-07 21:28:15 +0800

    update

--- csrc/shared_memory.hpp
+++ csrc/shared_memory.hpp
@@ -18,10 +18,32 @@ struct SharedMemory {
         }
         return ret;
     }
+    int py_shm_unlink(const char *name) {
+        int ret = shm_unlink(name);
+        if (ret == -1)
+            ret = errno;
+        return ret;
+    }
+    int py_ftruncate(int length) {
+        int ret = ftruncate(fd, (off_t)length);
+        if (ret == -1)
+            ret = errno;
+        return ret;
+    }
+    int py_fchmod(unsigned int mode) {
+        int ret = fchmod(fd, (mode_t)mode);
+        if (ret == -1)
+            ret = errno;
+        return ret;
+    }
 };
 
 void DEFINE_SHAREDMEMORY_MODULE(nb::module_ & (m)) {
     nb::class_<SharedMemory>(m, "SharedMemory")
         .def(nb::init<>())
-        .def("open", &SharedMemory::py_shm_open);
+        .def("open", &SharedMemory::py_shm_open)
+        .def("unlink", &SharedMemory::py_shm_unlink)
+        .def("ftruncate", &SharedMemory::py_ftruncate)
+        .def("fchmod", &SharedMemory::py_fchmod)
+        .def_prop_ro("fd", [](SharedMemory &shm) { return shm.fd; });
 }
